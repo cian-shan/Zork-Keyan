a5a18c158b24af11a15bf5e30e43a9f4c11d835d
Cian Shanahan <55852192+cian-shan@users.noreply.github.com>
2021-04-09 21:08:09 +0100

    Added appropriate rooms and entrances

--- ZorkUL.cpp
+++ ZorkUL.cpp
@@ -14,34 +14,61 @@ ZorkUL::ZorkUL() {
 }
 
 void ZorkUL::createRooms()  {
-	Room *a, *b, *c, *d, *e, *f, *g, *h, *i;
+    Room *n1, *n2, *n3, *n2e, *n2w, *n2e2, *n2w2, *s1, *s2, *s1e, *e1, *e2, *e3, *e1s, *e1n, *e2n, *c1, *w1;
 
-	a = new Room("a");
-        a->addItem(new Item("x", 1, 11));
-        a->addItem(new Item("y", 2, 22));
-	b = new Room("b");
-        b->addItem(new Item("xx", 3, 33));
-        b->addItem(new Item("yy", 4, 44));
-	c = new Room("c");
-	d = new Room("d");
-	e = new Room("e");
-	f = new Room("f");
-	g = new Room("g");
-	h = new Room("h");
-	i = new Room("i");
+    //North Set
+    n1 = new Room("n1");
+        n1->addItem(new Item("x", 1, 11));
+        n1->addItem(new Item("y", 2, 22));
+    n2 = new Room("n2");
+    n3 = new Room("n3");
+    n2e = new Room("n2e");
+    n2e2 = new Room("n2e2");
+    n2w = new Room("n2w");
+    n2w2 = new Room("n2w2");
+    //South Set
+    s1 = new Room("s1");
+    s2 = new Room("s2");
+    s1e = new Room("s1e");
+    //East Set
+    e1 = new Room("e1");
+    e2 = new Room("e2");
+    e3 = new Room("e3");
+    e1s = new Room("e1s");
+    e1n = new Room("e1n");
+    e2n = new Room("e2n");
+    //Centre
+    c1 = new Room("c1");
+    //West
+    w1 = new Room("w1");
 
 //             (N, E, S, W)
-	a->setExits(f, b, d, c);
-	b->setExits(NULL, NULL, NULL, a);
-	c->setExits(NULL, a, NULL, NULL);
-	d->setExits(a, e, NULL, i);
-	e->setExits(NULL, NULL, NULL, d);
-	f->setExits(NULL, g, a, h);
-	g->setExits(NULL, NULL, NULL, f);
-	h->setExits(NULL, f, NULL, NULL);
-    i->setExits(NULL, d, NULL, NULL);
+    // Centre
+    c1->setExits(n1, NULL, s1, e1);
+    // North Set
+    n1->setExits(n2, NULL, c1, NULL);
+    n2->setExits(n3, n2e, n1, n2w);
+    n3->setExits(NULL, NULL, n2, NULL);
+    n2e->setExits(NULL, NULL, n2e2, n2);
+    n2e2->setExits(n2e, NULL, NULL, NULL);
+    n2w->setExits(NULL, n2, n2w2, NULL);
+    n2w2->setExits(n1, NULL, NULL, NULL);
+    // South Set
+    s1->setExits(c1, s1e, s2, NULL);
+    s1e->setExits(NULL, NULL, NULL, s1);
+    s2->setExits(s1, NULL, NULL, NULL);
+    // East Set
+    e1->setExits(e1n, e2, e1s, c1);
+    e1s->setExits(n1, NULL, NULL, NULL);
+    e1n->setExits(NULL, NULL, e1, e2n);
+    e2->setExits(e2n, e3, NULL, e1);
+    e2n->setExits(NULL, NULL, e2, e1n);
+    e3->setExits(NULL, NULL, NULL, e2);
+    // West
+    w1->setExits(NULL, NULL, NULL, NULL);
 
-        currentRoom = a;
+
+        currentRoom = c1;
 }
 
 /**
