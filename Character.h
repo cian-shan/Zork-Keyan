c608ca4ad643450ed7e3ef9db502a6a1f9b768ba
Cian Shanahan <55852192+cian-shan@users.noreply.github.com>
2021-04-18 23:30:22 +0100

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,34 +1,23 @@
-#ifndef CHARACTER_H_
-#define CHARACTER_H_
-#include "Item.h"
-#include "food.h"
+#include "Food.h"
 
-#include <string>
-#include <sstream>
-using namespace std;
-#include <vector>
-using std::vector;
+Food::Food(string desc, int val, int dam, int key)
+    : Item(desc){
+    this->healValue = val;
+    this->damage = dam;
+    this->keys = key;
+}
+int Food::getValue(){
+    return this->healValue;
+}
 
+int Food::getDam(){
+    return this->damage;
+}
 
-class Character {
-private:
-	string description;
-    vector <Item *> itemsInCharacter;
+int Food::getKey(){
+    return this->keys;
+}
 
-public:
-    int health=50;
-    void addItem(Item *item);
-    void addItem(Item &item);
-
-public:
-    Character();
-	Character(string description);
-	string shortDescription();
-	string longDescription();
-    string name;
-    void setName(string name);
-    string getName();
-    string getHealth();
-};
-
-#endif
+void Food::operator ++(){
+    this->healValue+=20;
+}
